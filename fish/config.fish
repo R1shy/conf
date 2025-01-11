a80f0e46d0a732e407a33f3ac57eef4c51f42e4d
Emma Kadlecek <kadlecekemma@gmail.com>
2025-01-10 18:22:17 -0800

    fixed stupid issues

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
-source /home/r1shy/.config/fish/tokyonight_night.fish
+source /home/rishy/.config/fish/tokyonight_night.fish
 alias ls='ls -a'
 alias clock='tty-clock -stcB -C5'
 alias npm='sudo npm'
 export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="/home/r1shy/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
