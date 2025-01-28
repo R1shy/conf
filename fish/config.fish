3fd44171c56e0cbc924e81dcc9344d0fb96a2a52
Emma Kadlecek <kadlecekemma@gmail.com>
2025-01-27 18:58:44 -0800

    .gitignore update

--- fish/config.fish
+++
@@ -1,6 +0,0 @@
-source /home/rishy/.config/fish/tokyonight_night.fish
-alias ls='ls -a'
-alias clock='tty-clock -stcB -C5'
-alias reset='kill waybar && hyprctl dispatch exec waybar'
-export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="$HOME/.local/bin:$PATH"
