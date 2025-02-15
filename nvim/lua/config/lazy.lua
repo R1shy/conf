9692237913f2f40fbd0b8f9d3a55313200f2835f
Emma Kadlecek <kadlecekemma@gmail.com>
2025-02-14 20:28:51 -0800

    .

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
-  install = { colorscheme = { "toyonight" } },
+  install = { colorscheme = { "gruvbox" } },
   checker = { enabled = true },
 })
 require('config.autocmds')
