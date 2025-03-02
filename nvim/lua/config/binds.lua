bf6b54779382d7f9b4230d381608e834119c1857
Emma Kadlecek <kadlecekemma@gmail.com>
2025-03-02 14:48:17 -0800

    .

---
+++ nvim/lua/config/binds.lua
@@ -0,0 +1,15 @@
+local opts = {}
+local map = vim.keymap.set
+
+map("i", "<C-s>", "<Cmd>w<CR>", opts)
+map("n", "<C-s>", "<Cmd>w<CR>", opts)
+map('i', '<C-Left>', '<Cmd>BufferPrevious<CR>', opts)
+map('i', '<C-Right>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<C-Left>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<C-Right>', '<Cmd>BufferNext<CR>', opts)
+map('i', '<C-BS>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<C-BS>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
+map('i', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
+map('n', '<C-p>', '<Cmd>MarkdownPreviewToggle<CR>', opts)
+map('i', '<C-p>', '<Cmd>MarkdownPreviewToggle<CR>', opts)
---
+++ nvim/lua/config/line.lua
@@ -0,0 +1,11 @@
+local lualine = require('lualine')
+
+local config = {
+  options = {
+    component_separators = '',
+    section_separators = '',
+    theme = "gruvbox_dark"
+  }
+
+  }
+lualine.setup(config)
