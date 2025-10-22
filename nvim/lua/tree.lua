970957dbc93482569087cea99adaaf5a3476a22d
Rishy <kadlecekemma@gmail.com>
2025-10-22 16:05:55 -0700

    .

--- nvim/lua/tree.lua
+++ nvim/lua/tree.lua
@@ -3,5 +3,12 @@ require('pckr').add({
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    "3rd/image.nvim",
+    {
+    's1n7ax/nvim-window-picker',
+    tag = 'v2.*',
+    config = function()
+        require'window-picker'.setup()
+    end,
+}
 })
